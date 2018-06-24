34f88ce0ec749a0bc39372c2ebf1cad973784a54
Micha Kops <micha.kops@gmail.com>
2018-06-24 17:01:32 +0200

    Example for using ClassRules with RuleChains added.

--- src/test/java/com/hascode/tutorial/SomeUnitTestWithRuleChain.java
+++ src/test/java/com/hascode/tutorial/SomeUnitTestWithRuleChain.java
@@ -2,20 +2,48 @@ package com.hascode.tutorial;
 
 import static org.junit.Assert.assertTrue;
 
-import org.junit.Rule;
+import org.junit.ClassRule;
 import org.junit.Test;
+import org.junit.rules.ExternalResource;
 import org.junit.rules.RuleChain;
 import org.junit.rules.TestRule;
 
 public class SomeUnitTestWithRuleChain {
-	@Rule
-	public TestRule chain = RuleChain.outerRule(new ConsoleOutRule("outer"))
-			.around(new ConsoleOutRule("middle"))
-			.around(new ConsoleOutRule("inner"));
 
-	@Test
-	public void testSomeMethod() {
-		System.out.println("test started");
-		assertTrue(true);
-	}
+  @ClassRule
+  public static TestRule chain = RuleChain.outerRule(new ConsoleOutTestWrapper("outer"))
+      .around(new ConsoleOutTestWrapper("middle"))
+      .around(new ConsoleOutTestWrapper("inner"));
+
+  @Test
+  public void testSomeMethod1() {
+    System.out.println("test 1 started");
+    assertTrue(true);
+  }
+
+  @Test
+  public void testSomeMethod2() {
+    System.out.println("test 2 started");
+    assertTrue(true);
+  }
+
+  static class ConsoleOutTestWrapper extends ExternalResource {
+
+    private final String note;
+
+    ConsoleOutTestWrapper(String note) {
+      this.note = note;
+    }
+
+    @Override
+    protected void before() throws Throwable {
+      System.out.printf("[BEFORE] rule applied. note: %s%n", note);
+    }
+
+    @Override
+    protected void after() {
+      System.out.printf("[AFTER] rule applied. note: %s%n", note);
+    }
+  }
+
 }
